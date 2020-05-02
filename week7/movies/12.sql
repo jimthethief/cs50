93a53db05b81cd326fd5e2811a0f0c54d72e799d
Jamie Hamilton <jhamilton.contact@gmail.com>
2020-05-02 13:12:40 +0100

    Untrack unnecessary files in .gitignore

--- week7/movies/12.sql
+++
@@ -1,5 +0,0 @@
-SELECT movies.title FROM movies
-JOIN stars ON movies.id = stars.movie_id
-JOIN people ON stars.person_id = people.id
-WHERE people.name IN ('Johnny Depp', 'Helena Bonham Carter')
-GROUP BY movies.title HAVING COUNT(people.name) = 2;
--- week7/movies/9.sql
+++
@@ -1,4 +0,0 @@
-SELECT people.name FROM people WHERE people.id
-IN (SELECT stars.person_id FROM stars
-JOIN movies ON stars.movie_id = movies.id WHERE
-movies.year = '2004') ORDER BY people.birth;
